417231b899502df065fd74f25cb42b087975575a
Julio Bermudez <jbermudezfc90@gmail.com>
2020-04-30 19:20:53 -0400

    finished phonebook

--- src/App.js
+++ src/App.js
@@ -1,59 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Course from './components/Course';
 
 function App() {
-    const courses = [
-        {
-            name: 'Half Stack application development',
-            id: 1,
-            parts: [
-                {
-                    name: 'Fundamentals of React',
-                    exercises: 10,
-                    id: 1,
-                },
-                {
-                    name: 'Using props to pass data',
-                    exercises: 7,
-                    id: 2,
-                },
-                {
-                    name: 'State of a component',
-                    exercises: 14,
-                    id: 3,
-                },
-                {
-                    name: 'Redux',
-                    exercises: 11,
-                    id: 4,
-                },
-            ],
-        },
-        {
-            name: 'Node.js',
-            id: 2,
-            parts: [
-                {
-                    name: 'Routing',
-                    exercises: 3,
-                    id: 1,
-                },
-                {
-                    name: 'Middlewares',
-                    exercises: 7,
-                    id: 2,
-                },
-            ],
-        },
-    ];
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', phone: '040-123456' },
+        { name: 'Ada Lovelace', phone: '39-44-5323523' },
+        { name: 'Dan Abramov', phone: '12-43-234345' },
+        { name: 'Mary Poppendieck', phone: '39-23-6423122' },
+    ]);
+    const [newName, setNewName] = useState('');
+    const [newPhone, setNewPhone] = useState('');
+    const [filter, setFilter] = useState('');
+
+    function addPerson(e) {
+        e.preventDefault();
+        const names = persons.map((person) => person.name);
+        names.includes(newName)
+            ? alert(`${newName} is already added to the phonebook`)
+            : setPersons(persons.concat({ name: newName, phone: newPhone }));
+        setNewName('');
+        setNewPhone('');
+    }
+
+    const filteredPhonebook = persons.filter((person) =>
+        person.name.toLocaleLowerCase().includes(filter.toLowerCase()),
+    );
+
+    const showThis = filter ? filteredPhonebook : persons;
 
     return (
         <div>
-            {courses.map((course) => (
-                <Course key={course.id} course={course} />
+            <h2>Phonebook</h2>
+            <Filter filter={filter} onChange={(e) => setFilter(e.target.value)} />
+            <AddForm
+                onSubmit={addPerson}
+                newName={newName}
+                newPhone={newPhone}
+                setNewName={(e) => setNewName(e.target.value)}
+                setNewPhone={(e) => setNewPhone(e.target.value)}
+            />
+            <h2>Numbers</h2>
+            {showThis.map((person) => (
+                <Person key={person.name} person={person} />
             ))}
         </div>
+    );
+}
+
+function AddForm({ onSubmit, newName, newPhone, setNewName, setNewPhone }) {
+    return (
+        <form onSubmit={onSubmit}>
+            <h3>add new</h3>
+            <div>
+                name: <input value={newName} onChange={setNewName} />
+                <br />
+                <br />
+                phone: <input value={newPhone} onChange={setNewPhone} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    );
+}
+
+function Filter({ filter, onChange }) {
+    return (
+        <p>
+            find person <input value={filter} onChange={onChange} />
+        </p>
+    );
+}
+
+function Person({ person }) {
+    return (
+        <p>
+            {person.name}, {person.phone}
+        </p>
     );
 }
 
